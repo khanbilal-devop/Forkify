f1a83f799e3e84eef2b2cae18106f3087153bc76
Khan Ahmed <khan.ahmed@dspim.com>
2023-08-26 17:01:31 +0530

    Fetching of recipe from api and rendering the same

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,155 @@
+import icons from 'url:../img/icons.svg';
+import { showSpinner, hideSpinner ,showError} from './util';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
 
-// https://forkify-api.herokuapp.com/v2
+//https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+
+const fetchRecipie = async (id) => {
+  let recipe = {};
+  try {
+    showSpinner(recipeContainer);
+    const response =
+      await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+    const data = await response.json();
+    if (!response.ok) throw new Error(`Status : ${response?.status} \n${data?.message}`)
+    recipe = data?.data?.recipe;
+    recipe = {
+      id: recipe?.id,
+      title: recipe?.title,
+      publisher: recipe?.publisher,
+      sourceUrl: recipe?.source_url,
+      image: recipe?.image_url,
+      servings: recipe?.servings,
+      cookingTime: recipe?.cooking_time,
+      ingredients: recipe?.ingredients
+    }
+  } catch (err) {
+    hideSpinner(recipeContainer);
+    recipe = {};
+    showError('No recipes found for your query. Please try again!',recipeContainer)
+  }
+  return recipe;
+}
+
+
+const renderRecipie = async (id= '5ed6604591c37cdc054bc886') => {
+  const recipe = await fetchRecipie(id);
+  if (Object.keys(recipe).length) {
+    const { image, title, cookingTime, servings, ingredients } = recipe
+    const markup = `
+   <figure class="recipe__fig">
+   <img src="${image}" alt="${title}" class="recipe__img" />
+   <h1 class="recipe__title">
+     <span>${title}</span>
+   </h1>
+ </figure>
+
+ <div class="recipe__details">
+   <div class="recipe__info">
+     <svg class="recipe__info-icon">
+       <use href="${icons}#icon-clock"></use>
+     </svg>
+     <span class="recipe__info-data recipe__info-data--minutes">${cookingTime}</span>
+     <span class="recipe__info-text">minutes</span>
+   </div>
+   <div class="recipe__info">
+     <svg class="recipe__info-icon">
+       <use href="${icons}#icon-users"></use>
+     </svg>
+     <span class="recipe__info-data recipe__info-data--people">${servings}</span>
+     <span class="recipe__info-text">servings</span>
+
+     <div class="recipe__info-buttons">
+       <button class="btn--tiny btn--increase-servings">
+         <svg>
+           <use href="${icons}#icon-minus-circle"></use>
+         </svg>
+       </button>
+       <button class="btn--tiny btn--increase-servings">
+         <svg>
+           <use href="${icons}#icon-plus-circle"></use>
+         </svg>
+       </button>
+     </div>
+   </div>
+
+   <div class="recipe__user-generated">
+     <svg>
+       <use href="${icons}#icon-user"></use>
+     </svg>
+   </div>
+   <button class="btn--round">
+     <svg class="">
+       <use href="${icons}#icon-bookmark-fill"></use>
+     </svg>
+   </button>
+ </div>
+
+ <div class="recipe__ingredients">
+   <h2 class="heading--2">Recipe ingredients</h2>
+   <ul class="recipe__ingredient-list">
+      ${(ingredients || []).map(each =>
+      `<li class="recipe__ingredient">
+        <svg class="recipe__icon">
+          <use href="${icons}#icon-check"></use>
+        </svg>
+        <div class="recipe__quantity">${each?.quantity}</div>
+        <div class="recipe__description">
+          <span class="recipe__unit">${each?.unit}</span>
+          ${each?.description}
+        </div>
+      </li>`
+    ).join("")}
+  
+
+     <li class="recipe__ingredient">
+       <svg class="recipe__icon">
+         <use href="${icons}#icon-check"></use>
+       </svg>
+       <div class="recipe__quantity">0.5</div>
+       <div class="recipe__description">
+         <span class="recipe__unit">cup</span>
+         ricotta cheese
+       </div>
+     </li>
+   </ul>
+ </div>
+
+ <div class="recipe__directions">
+   <h2 class="heading--2">How to cook it</h2>
+   <p class="recipe__directions-text">
+     This recipe was carefully designed and tested by
+     <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
+     directions at their website.
+   </p>
+   <a
+     class="btn--small recipe__btn"
+     href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+     target="_blank"
+   >
+     <span>Directions</span>
+     <svg class="search__icon">
+       <use href="${icons}#icon-arrow-right"></use>
+     </svg>
+   </a>
+ </div>
+   `
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML("afterbegin", markup);
+  }
+}
+
+const init = () => {
+  window.addEventListener('hashchange',(e) => {
+  let id = (e.target.location.hash).substring('1');
+  renderRecipie(id);
+});}
+
+init();
