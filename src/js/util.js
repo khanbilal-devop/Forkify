1992143b2da7b31b5a822b4b19263d36dce9c34f
Khan Ahmed <khan.ahmed@dspim.com>
2023-08-30 23:31:41 +0530

    Refactored to Mvc Pattern architecture

--- src/js/util.js
+++ src/js/util.js
@@ -1,37 +1,4 @@
-import icons from 'url:../img/icons.svg';
 
 
-const showSpinner = (targetElement) => {
-    const spinner = `
-    <div class="spinner">
-    <svg>
-      <use href="${icons}#icon-loader"></use>
-    </svg>
-  </div> 
-    `
-    targetElement.textContent = '';
-    targetElement.insertAdjacentHTML("afterbegin", spinner);
-}
 
-const hideSpinner = (targetElement) => {
-    const isSpinnerPresent = targetElement.querySelectorAll(":is(div).spinner").length;
-    if (isSpinnerPresent) {
-        targetElement.textContent = '';
-    }
-}
-
-
-const showError = (errorMessage = 'Something went wrong',targetElement) => {
-    const errorHtml = ` <div class="error">
-    <div>
-      <svg>
-        <use href="${icons}#icon-alert-triangle"></use>
-      </svg>
-    </div>
-    <p>${errorMessage}</p>
-  </div>`
-  targetElement.insertAdjacentHTML("afterbegin", errorHtml);
-
-}
-
-module.exports = { showSpinner, hideSpinner ,showError}
+// module.exports = { }
