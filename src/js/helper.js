7ed4e8ec9d64a6fdcdf3c8e284870575f44d99e0
Khan Ahmed <khan.ahmed@dspim.com>
2023-08-31 20:39:18 +0530

    New helper method created

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,9 +1,19 @@
+import { TIME_OUT_SECONDS } from "./constant";
 
 
-export const timeout =  (s) => {
+export const timeout = (s) => {
     return new Promise(function (_, reject) {
-      setTimeout(function () {
-        reject(new Error(`Request took too long! Timeout after ${s} second`));
-      }, s * 1000);
+        setTimeout(function () {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
     });
-  };
+};
+
+
+export const getJson = async (url) => {
+   const response = await Promise.race([fetch(url), timeout(TIME_OUT_SECONDS)]);
+    // 5ed6604591c37cdc054bc880
+    const data = await response.json();
+    if (!response.ok) throw new Error(`Status : ${response?.status} \n${data?.message}`)
+    return data;
+}
